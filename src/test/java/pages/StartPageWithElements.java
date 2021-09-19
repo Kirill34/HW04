1d0314ae2a8d321a097e098b09727d755c6cf4f8
KKS <kirill.kuliuckin2016@yandex.ru>
2021-09-19 04:02:29 +0300

    Class for StartPage

---
+++ src/test/java/pages/StartPageWithElements.java
@@ -0,0 +1,61 @@
+package pages;
+
+import elements.Link;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+// Стартовая страница сайта DNS
+public class StartPageWithElements extends BasePage {
+    // Логгер
+    private Logger logger = LogManager.getLogger(StartPageWithElements.class);
+    // URL страницы
+    private final String URL = "https://www.dns-shop.ru/";
+
+    // ***** Локаторы *****
+    // Кнопка "Да" на всплывашке
+    String linkYesXpath = "//a[contains(text(),\"Да\")]";
+    // Ссылка "Смартфоны и гаджеты"
+    String linkTVAndMultimediaXpath = "(//a[contains(text(), \"ТВ и мультимедиа\")])[1]";
+    // Ссылка "Смартфоны"
+    String linkTVsXpath = "(//a[contains(text(), \"Телевизоры\")])[2]/following::div/a";
+
+    // Конструктор класса
+    public StartPageWithElements(WebDriver driver) {
+        // Вызов родительского конструктора
+        super(driver);
+    }
+
+    // Получение URL страницы
+    public String getURL() {
+        return this.URL;
+    }
+
+    // Открытие страницы
+    public void openPage() {
+        driver.get(this.URL);
+        logger.info("Открыта страница https://www.dns-shop.ru/");
+    }
+
+    // Нажатие на кнопку "Да"
+    public void linkYesClick() {
+        Link linkYes = new Link(driver, By.xpath(linkYesXpath));
+        linkYes.click();
+        logger.info("Нажата кнопка \"Да\"");
+    }
+
+    // Наведение курсора мыши на ссылку "ТВ и мультимедиа"
+    public void linkSmartsAndGadgetsMove() {
+        Link linkTVAndMultimedia = new Link(driver, By.xpath(linkTVAndMultimediaXpath));
+        linkTVAndMultimedia.focusOnLink();
+        logger.info("Курсор мыши наведен на ссылку \"Смартфоны\"");
+    }
+
+    // Нажатие на ссылку "Телевизоры"
+    public void linkSmartsClick() {
+        Link linkTVs = new Link(driver, By.xpath(linkTVsXpath));
+        linkTVs.click();
+        logger.info("Нажата ссылка \"Смартфоны\"");
+    }
+}
