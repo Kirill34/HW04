68c116be703e2cf0f91b5f305796ecb1766beb28
KKS <kirill.kuliuckin2016@yandex.ru>
2021-09-19 14:58:10 +0300

    Img element

---
+++ src/test/java/elements/Img.java
@@ -0,0 +1,10 @@
+package elements;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class Img extends BaseElement{
+    public Img(WebDriver driver, By by) {
+        super(driver, by);
+    }
+}
